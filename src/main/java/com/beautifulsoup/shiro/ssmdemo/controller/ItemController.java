7a46ae3e267b058cf678d75b9a869bd07cf2d202
Fuyunwang <2375872953@qq.com>
2017-12-16 22:17:24 +0800

    添加缓存支持

--- src/main/java/com/beautifulsoup/shiro/ssmdemo/controller/ItemController.java
+++ src/main/java/com/beautifulsoup/shiro/ssmdemo/controller/ItemController.java
@@ -1,5 +1,6 @@
 package com.beautifulsoup.shiro.ssmdemo.controller;
 
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ItemController {
 	
 	@RequestMapping("/delete02")
+	@RequiresPermissions("item:delete:02")
 	public String itemDelete(){
 		
 		
 		return "delete02item";
 	}
 	
+	@RequiresPermissions("item:query")
 	@RequestMapping("/query")
 	public String ItemQuery(){
 		return "itemquery";
